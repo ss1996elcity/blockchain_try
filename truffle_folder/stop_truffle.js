a97b249237331fa14db9fb7b0a9680d708502e53
Sergey SS <Dor7la96@mail.ru>
2022-05-13 01:43:57 +0300

    Trying CI/CD. Test #48

--- truffle_folder/stop_truffle.js
+++ truffle_folder/stop_truffle.js
@@ -1,9 +1,2 @@
-//const { exec } = require("child_process");
-//exec("killall node");
-
-function exec(cmd, handler = function(error, stdout, stderr){console.log(stdout);if(error !== null){console.log(stderr)}})
-{
-    const childfork = require('child_process');
-    return childfork.exec(cmd, handler);
-}
-exec('killall node');
+const { exec } = require("child_process");
+exec("killall -s 9 node");
