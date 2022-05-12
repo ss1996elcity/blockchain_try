70567f65bfd0808c0363f53a10a313ad1866d9f2
Sergey SS <Dor7la96@mail.ru>
2022-05-12 19:25:08 +0300

    Trying CI/CD. Test #20

--- truffle_folder/test/TestCryptoZombies.js
+++ truffle_folder/test/TestCryptoZombies.js
@@ -3,11 +3,11 @@ contract("SafeMath_ADD test", async accounts => {
     it("result_add should return 7", async() => {
         var instance = await CryptoZombies.deployed();
         var result_add = await instance.my_add(6);
-        assert.equal(result_add.valueOf(), 7);
+        assert.equal(result_add, 7);
     });
     it("result_sub should return 39", async() => {
         var instance = await CryptoZombies.deployed();
         var result_sub = await instance.my_sub(40);
-        assert.equal(result_sub.valueOf(), 39);
+        assert.equal(result_sub, 39);
     });
 });
