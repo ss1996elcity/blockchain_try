bc6ea4c2430fcb6556ca81d07f0e3affac8a4ec3
Sergey SS <Dor7la96@mail.ru>
2022-05-12 19:35:51 +0300

    Trying CI/CD. Test #22

--- truffle_folder/test/TestCryptoZombies.js
+++ truffle_folder/test/TestCryptoZombies.js
@@ -2,12 +2,12 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 contract("SafeMath_ADD test", async accounts => {
     it("result_add should return 7", async() => {
         var instance = await CryptoZombies.deployed();
-        var result_add = await instance.my_add(6);
+        var result_add = await instance.my_add.call(6);
         assert.equal(result_add.valueOf(), 7);
     });
     it("result_sub should return 39", async() => {
         var instance = await CryptoZombies.deployed();
-        var result_sub = await instance.my_sub(40);
+        var result_sub = await instance.my_sub.call(40);
         assert.equal(result_sub.valueOf(), 39);
     });
 });
