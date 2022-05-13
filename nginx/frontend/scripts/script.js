852a0eef6e36ba8983079cc12f846fac350aa451
Sergey SS <Dor7la96@mail.ru>
2022-05-13 17:30:45 +0300

    Trying CI/CD. Test #58

--- nginx/frontend/scripts/script.js
+++ nginx/frontend/scripts/script.js
@@ -1,9 +1,10 @@
-import { sum, difference, product, quotient } from './functions.js'
+//import { sum, difference, product, quotient } from './functions.js'
+import * as mathFunctions from './functions.js'
 const x = 10
 const y = 5
 document.getElementById('x').textContent = x
 document.getElementById('y').textContent = y
-document.getElementById('addition').textContent = sum(x, y)
-document.getElementById('subtraction').textContent = difference(x, y)
-document.getElementById('multiplication').textContent = product(x, y)
-document.getElementById('division').textContent = quotient(x, y)
+document.getElementById('addition').textContent = mathFunctions.sum(x, y)
+document.getElementById('subtraction').textContent = mathFunctions.difference(x, y)
+document.getElementById('multiplication').textContent = mathFunctions.product(x, y)
+document.getElementById('division').textContent = mathFunctions.quotient(x, y)
